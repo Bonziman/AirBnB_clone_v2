f845bb02f33a95be541e253c56be2e6b08e75489
Aymane Eloirdiwi <103787921+Bonziman@users.noreply.github.com>
2024-04-28 17:53:06 +0100

    sql commnt

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+--script to create db and user ang give privileges
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost';
