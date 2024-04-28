cf4ac7ccf7594870bcaed3ea01e88c7f09486faa
Aymane Eloirdiwi <103787921+Bonziman@users.noreply.github.com>
2024-04-28 17:57:01 +0100

    db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,9 @@
 IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'hbnb_dev_db')
   BEGIN
     CREATE DATABASE hbnb_dev_db
+    END
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
 
