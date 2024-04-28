cc43e79223d021156a98b1ff9b17335e7fdc19cb
Aymane Eloirdiwi <103787921+Bonziman@users.noreply.github.com>
2024-04-28 17:59:59 +0100

    flush

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,14 @@
---script to create db and user ang give privileges
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'hbnb_dev_db')
-BEGIN
-  CREATE DATABASE hbnb_dev_db;
-END;
-GO
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the database hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
-
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
