008febe3a120e29e715281e71b3e30ab92ccaf55
Bonziman <eloirdiwi@gmail.com>
2024-05-16 21:11:00 +0100

    first simple app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""model that defines a simple hello world flask app"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello HBNB!"
